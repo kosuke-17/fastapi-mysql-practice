1379eaa844ddce8c3c3b705b0e08cc7c3e169250
kosuke-17 <tamutamu17@icloud.com>
2023-08-08 15:29:16 +0900

    postするapiを作成

--- app/python/main.py
+++ app/python/main.py
@@ -1,4 +1,6 @@
+from enum import Enum
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -11,3 +13,21 @@ async def say_hallo():
 @app.get("/api/{message}")
 async def get_message(message: str):
     return {"message": message}
+
+
+class SexType(str, Enum):
+    Male = "男"
+    Female = "女"
+
+
+class SchemaofTitanicFeaturesRequest(BaseModel):
+    Sex: SexType
+    Pclass: str
+    Age: int
+    Parch: int
+    SibSp: int
+
+
+@app.post("/api/titanic", response_model=SchemaofTitanicFeaturesRequest)
+def derive_score(body: SchemaofTitanicFeaturesRequest):
+    return body
