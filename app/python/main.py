2e86a76133a903b536ef90bab571e9ff50538820
kosuke-17 <tamutamu17@icloud.com>
2023-08-08 15:20:29 +0900

    helloworldを表示させるapiを作成

--- app/python/main.py
+++ app/python/main.py
@@ -1,3 +1,13 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+
+
+@app.get("/helloworld")
+async def say_hallo():
+    return {"message": "Hello World"}
+
+
+@app.get("/api/{message}")
+async def get_message(message: str):
+    return {"message": message}
